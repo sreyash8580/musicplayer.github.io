c8f301283e0ce94c663366d1036fd22fd6360a73
sreyash8580 <78336924+sreyash8580@users.noreply.github.com>
2021-07-04 15:57:00 +0530

    Update script.js
    Added progres bar and timer

--- script.js
+++ script.js
@@ -1,185 +1,155 @@
-/* @import url('https://fonts.googleapis.com/css2?family=Jost:wght@100&display=swap'); */
+ // const music = document.querySelector("audio");
 
-/* font-family: 'Jost', sans-serif; */
+    const music = document.querySelector("audio");
+    const img = document.querySelector("img");
+    const play = document.getElementById("play");
 
-*,
-*::before,
-*::after{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+    const artist = document.getElementById("artist");
+    const title = document.getElementById("title");
+    const prev = document.getElementById("prev");
+    const next = document.getElementById("next");
 
-
-html{
-    font-size: 62.5%;
-    font-family: 'Jost', sans-serif;
-}
-
-.main_div{
-    width: 100vw;
-    height: 100vh;
-    /* background-color: #f6f6f6; */
-    background-image: url(images/bg/bg1.png);
-    /* opacity: 0.5; */
-    /* div ke andar div ko center krne k liye niche wala use kro  */
-    display: grid;
-    place-items: center;
-}
-
-/* 1 rem = 10 px  */
-
-.music_container{
-    width: 35rem;
-    height: 55rem;
-    background-color: #a47e7e;
-    /* background-image: linear-gradient(0 0 0 rgba 0.4); */
-    border-radius: 2rem;
-    box-shadow: 0 1.2rem 3rem 0.5rem rgba(0, 0, 0, 0.2);
-    padding: 3rem;
-    text-align: center;
-}
-
-.music_container #title{
-    text-transform: uppercase;
-    letter-spacing: 0.2rem;
-    word-spacing: 0.5rem;
-    color: #171717;
-    margin: 2rem 0 0.5rem 0;
-    font-size: 2.5rem;
-    font-weight: 500;
-    text-shadow: 0 0.3rem 0.5rem rgba(0, 0, 0, 0.3);
-}
-
-.music_container #artist{
-    /* color: #cccaca; */
-    color: #504141;
-    text-transform: capitalize;
-    letter-spacing: 0.1rem;
-    font-size: 2rem;
-    margin-bottom: 4rem;
-    font-weight: 300;
-}
-
-.img_container{
-    width: 25rem;
-    height: 25rem;
-    margin: auto;
-}
-
-.img_container img{
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    object-fit: cover; /* parent div k andar fit krne k liye*/
-    box-shadow: 0 1.2rem 3rem 0.5rem rgba(0, 0, 0, 0.4);
-}
-
-.music_controls{
-    width: 20rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: auto;
-    margin-top: 1rem;
-}
-
-.music_controls .fas{
-    color: #111111;
-    font-size: 2rem;
-    cursor: pointer;
-    filter: drop-shadow(0 1.2rem 3rem rgba(0, 0, 0, 0.4));
-}
+    let progress= document.getElementById("progress");
+    let total_duration = document.getElementById("duration"); 
+    let current_time = document.getElementById("current_time");
+    let progress_div = document.getElementById("progress_div");
 
 
-.music_controls .main_button{
-    width: 5rem;
-    height: 5rem;
-    border-radius: 50%;
-    /* background-color: rgb(224, 208, 208); */
-    background-color: #111;
-    color: #f6f6f6;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1.4rem;
-}
+    // Array of objects to declare songs 
+    const songs = [
+        {
+        name:"1",
+        title:"Kya mujhe pyar hai",
+        artist:"KK",
+        },
 
+        {
+        name:"2",
+        title:" Dilbara ",
+        artist:"B.Praak",
+        },
 
-/* fontawesome hover  */
+        {
+        name:"3",
+        title:"Hum Tum",
+        artist:"Sukriti Kakar, Prakriti Kakar",
+        },
+    ];
 
-.music_controls .fas:hover{
-    color: grey;
-}
+    
+    let isPlaying = false;
+// PLAY 
+    // play.addEventListener("click", () => {
+        const playMusic = () => {
+        isPlaying = true;
+        music.play();
+        play.classList.replace("fa-play", "fa-pause") //To replace play-pause button on click
+        img.classList.add("anime");
+    // });
+    };
 
-/* play button hover  */
+// PAUSE 
+    // play.addEventListener("click", () => {
+        const pauseMusic = () => {
+        isPlaying = false;
+        music.pause();
+        play.classList.replace("fa-pause", "fa-play")
+        img.classList.remove("anime");
+    // });
+    };
+    
+    play.addEventListener("click", () => {
+        if(isPlaying)
+        {
+            pauseMusic();
+        }
+        else{
+            playMusic();
+        }
 
-.music_controls .fa-play:hover{
-    background-color: #f6f6f6;
-    color: #111;
-    box-shadow: 0 1rem 2rem 0.2rem rgba(0, 0, 0, 0.4);
-}
+        // TERNANRY OPERATOR isPlaying ? pauseMusic() : playMusic();
+    });
 
-/* add animation class to animate */
+    // Changing Music 
+    const loadSong = (songs) => { 
+        title.textContent=songs.title;
+        artist.textContent=songs.artist;
+        music.src = "audio/" + songs.name + ".mp3"; 
+        img.src = "images/" + songs.name + ".png";
+    };
 
-.anime{
-     animation: rotatePlayer 3s linear infinite;
-}
+    songsIndex = 0;
+    //  loadSong(songs[1]);
 
-@keyframes rotatePlayer{
-    from{ 
-        transform: rotate(0deg);
-    }
-    to{
-        transform: rotate(360deg);
-    }
-}
+    //  next song
+    const nextSong = () => {
+        // songsIndex++;
+        songsIndex = (songsIndex + 1) % songs.length; // Songs khatam hone ke baad first me automatic jane k liye
+        loadSong(songs[songsIndex]);
+        playMusic();
+    };
 
-/* PROGRESS STYLE  */
-.progressbar_container{
-    width: 100%;
-    height: auto;
-    padding: 1rem 0;
-    margin-top: 1rem;
-}
+    // previous song
+    const prevSong = () => {
+        // songsIndex++;
+        songsIndex = (songsIndex - 1 + songs.length) % songs.length;
+        loadSong(songs[songsIndex]);
+        playMusic();
+    };
 
-.progress_div{
-    width: 100%;
-    height: 0.5rem;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07), 0 2px 4px rgba(0, 0, 0, 0.07), 0 4px 8px rgba(0, 0, 0, 0.07), 0 8px 16px rgba(0, 0, 0, 0.07),
-    0 16px 32px rgba(0, 0, 0, 0.07), 0 32px 64px rgba(0, 0, 0, 0.07);
-    border-radius: 4rem;
-    position: relative;
-    margin-top: 0.5rem;
-    transition: all 1s linear;
-    -moz-transition: all 1s linear;
-    -o-transition: all 1s linear;
-    -webkit-transition: all 1s linear;
-    cursor: pointer;
-    -webkit-appearance: none;
-    appearance: none;
-}
+    // PROGRESS DURATION 
+    music.addEventListener('timeupdate',(event) => {
+     // console.log(event);
+     const { currentTime, duration} = event.srcElement;
+     // console.log(currentTime);
+     // console.log(duration);
 
-/* BLACk LINE IN PROGRESS BAR  */
-.progress{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0%;
-    height: 100%;
-    background-color: black; 
-    border-radius: 4rem;
-    /* transition: width 1s linear;
-    -moz-transition: width 1s linear;
-    -o-transition: width 1s linear;
-    -webkit-transition: width 1s linear; */
-    cursor: pointer;
-    -webkit-appearance: none;
-    appearance: none;
-}
+     let progress_time = (currentTime / duration) * 100;
+        progress.style.width = `${progress_time}%`;
+        // % sign to seek that black line with duration
 
-.progress_duration{
-    display: flex;
-    justify-content: space-between;
-    font-size: 1.2rem;
-}
+        // Update music duration dynamically
+        let minute_duration = Math.floor(duration/60);
+        let second_duration = Math.floor(duration%60);
+        // console.log(minute_duration);
+        // console.log(second_duration);
+
+        let tot_duration = `${minute_duration}:${second_duration}`;
+        // to avoid NAN text while changing song use if condition
+        if(duration){
+            total_duration.textContent= `${tot_duration}`;
+        }
+        // total_duration.textContent = `${tot_duration}`;
+    
+
+        // Currrent music duration dynamically
+        let minute_currentTime = Math.floor(currentTime/60);
+        let second_currentTime = Math.floor(currentTime%60);
+
+        if(second_currentTime < 10){
+            second_currentTime = `0${second_currentTime}`;
+        }
+        let tot_currentTime = `${minute_currentTime}:${second_currentTime}`;
+        current_time.textContent = `${tot_currentTime}`;
+    });
+
+    // PROGRESS BAR CLICK 
+    progress_div.addEventListener("click", (event) => {
+        console.log(event); 
+        const {duration} = music;
+
+        let  move_progress = (event.offsetX / event.srcElement.clientWidth) * duration;
+        // console.log(move_progress); 
+         
+        music.currentTime = move_progress;
+    });
+
+    // NEXT SONG IF SONG ENDS 
+    music.addEventListener("ended", nextSong);
+
+    //  next button
+    next.addEventListener("click", nextSong);
+    
+    // previous button
+    prev.addEventListener("click", prevSong);
+ 
